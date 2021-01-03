c7caa4ea39691ee5689d7bf99029dff0a4f361ac
Jatnael Montes <ifelsejet@gmail.com>
2021-01-02 16:52:53 -0800

    Got unity scene set up

---
+++ GameClient/Assets/Scripts/Client.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour
+{
+    public static Client instance;
+    public static int dataBufferSize = 4096;
+
+    public string ip = "127.0.0.1";
+    public int port = 26950;
+    public int myId = 0;
+    public TCP tcp;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != null)
+        {
+            Debug.log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        tcp = new TCP();
+    }
+    
+    public void ConnectToServer()
+    {
+        tcp.Connect();
+    }
+
+    public class TCP
+    {
+        public TcpClient socket;
+
+        private NetworkStream stream;
+        private byte[] recieveBuffer;
+
+        public void Connect()
+        {
+            socket = new TcpClient
+            {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            recieveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
+        }
+        private void ConnectCallback(IAsyncResult _result)
+        {
+            socket.EndConnect(_result);
+            if (!socket.Connected)
+            {
+                return; 
+            }
+            stream = socket.GetStream();
+
+            stream.BeginRead(recieveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+        private void RecieveCallback(IAsyncResult _result)
+        {
+            try
+            {
+                int _byteLength = stream.EndRead(_result);
+                if(_byteLength <= 0)
+                {
+                    return;
+                }
+                byte[] _data = new byte[_byteLength];
+                Array.Copy(recieveBuffer, _data, _byteLength);
+                stream.BeginRead(recieveBuffer, 0, dataBufferSize, RecieveCallback, null);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+}
